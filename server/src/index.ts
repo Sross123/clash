5cdcdecab97b4d784d2d40a3e7a3bcb6ae0eb4a1
Shashi <shashiross9@gmail.com>
2024-12-26 11:38:46 +0530

    setup email template and other things

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,29 @@
 import express, { Application, Request, Response } from "express";
 import "dotenv/config";
+import ejs from 'ejs'
 
 const app: Application = express();
 const PORT = process.env.PORT || 7000;
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { sendMail } from "./config/mail.js";
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
-app.get("/", (req:Request, res:Response)=>{
-    res.send("Server is running")
+// setup ejs form sending mail
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// setup view engine
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "./views"));
+
+app.get("/", async (req: Request, res: Response) => {
+    // we will use ejs to send html tag and send email
+    const html = await ejs.renderFile(__dirname + `/views/emails/welcome.ejs`, { name: "Shashi Ross", });
+    await sendMail("yefelo5836@kelenson.com", "Testing SMTP", html)
+    res.send("Email sent successfully");
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-});
+}); 
