b3cb04c78dcf02f93b7e0722489dee0d8b575271
a-s-t-e-y-a <krishnoit100@gmail.com>
2024-12-26 11:51:38 +0530

    FIXED

--- server/src/config/mail.ts
+++ server/src/config/mail.ts
@@ -1,20 +1,20 @@
-// import nodemailer, {TransportOptions} from 'nodemailer'
+import nodemailer, {TransportOptions} from 'nodemailer'
 
-// export const transporter = nodemailer.createTransport({
-//     host: process.env.SMTP_HOST,
-//     port: process.env.SMTP_PORT || 465,
-//     secure: process.env.SMTP_SECURE_FLAG || true,
-//     auth: {
-//         user: process.env.SMTP_USER,
-//         pass: process.env.SMTP_PASSWORD,
-//     },
-// } as TransportOptions)
+export const transporter = nodemailer.createTransport({
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT || 465,
+    secure: process.env.SMTP_SECURE_FLAG || true,
+    auth: {
+        user: process.env.FROM_EMAIL,
+        pass: process.env.SMTP_PASSWORD,
+    },
+} as TransportOptions)
 
-// export const sendMail = async (to:string, subject:string, body:string) => {
-//     await transporter.sendMail({
-//         from: process.env.FROM_EMAIL, // sender addressa
-//         to: to, // list of receivers
-//         subject: subject, // Subject line
-//         html: body, // html body
-//     });
-// }
+export const sendMail = async (to:string, subject:string, body:string) => {
+    await transporter.sendMail({
+        from: process.env.FROM_EMAIL, // sender addressa
+        to: to, // list of receivers
+        subject: subject, // Subject line
+        html: body, // html body
+    });
+}
